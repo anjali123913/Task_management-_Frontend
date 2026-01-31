3a82d12d7816a8c94e083d570c67e03b3c5f6cc0
anjali123913 <anjliverma95062@gmail.com>
2026-01-31 21:36:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const API_URL = "http://localhost:5000/api/tasks";
+const taskList = document.getElementById("taskList");
+
+const form = document.getElementById("taskForm");
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const taskStatus = document.getElementById("taskStatus"); // ✅ fixed ID
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const task = {
+    title: title.value.trim(),
+    description: description.value.trim(),
+    status: taskStatus.value
+  };
+
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(task)
+  });
+
+  if (res.ok) {
+    form.reset();
+    loadTasks();
+  } else {
+    alert("Error adding task!");
+  }
+  console.log(res
+
+  )
+});
+
+
+
+// Function to load tasks
+async function loadTasks() {
+  const res = await fetch(API_URL);
+  const tasks = await res.json();
+
+  taskList.innerHTML = "";
+
+  tasks.forEach(task => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      <div class="task-info">
+        <h3>${task.title}</h3>
+        <p>${task.description || "<i>No description</i>"}</p>
+      </div>
+      <div class="task-actions">
+        <span class="status-badge" data-status="${task.status}"></span>
+        <button onclick="deleteTask('${task._id}')">Delete</button>
+      </div>
+    `;
+
+    taskList.appendChild(li);
+  });
+}
+
+// Delete Task
+async function deleteTask(id) {
+  await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+  loadTasks();
+}
+
+// Initial load
+loadTasks();
