b569261d21bce553edbaafea8b85fe1986c7f0d7
anjali123913 <anjliverma95062@gmail.com>
2026-01-31 21:46:32 +0530

    api url change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = "https://task-management-r9nn.onrender.com/api/tasks";
 const taskList = document.getElementById("taskList");
 
 const form = document.getElementById("taskForm");
